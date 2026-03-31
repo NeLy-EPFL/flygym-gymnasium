d6a03facaf2fb594ca0dc47920b6a9f370804266
Sibo Wang <sibo.wang@epfl.ch>
2026-03-31 11:59:42 +0200

    formatting

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -90,4 +90,3 @@ html_theme_options = {
 html_favicon = "_static/favicon.ico"
 html_title = "NeuroMechFly documentation"
 html_logo = "https://raw.githubusercontent.com/NeLy-EPFL/_media/refs/heads/main/flygym/banner_small.jpg"
-
